9a3563c65396b8c34d08331bed59a461c7f08f0f
jbinalingbing-del <jbinalingbing@gbox.ncf.edu.ph>
2025-12-11 09:08:53 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+
+    if (window.scrollY > 50) {
+        navbar.style.backgroundColor= 'rgba(10, 10, 10, 0.9)';
+    } else {
+        navbar.style.backgroundColor = 'rgba(10, 10, 10, 0)';
+    }
+});
